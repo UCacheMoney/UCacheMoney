fe7baf0e978fa789989372327852263db4f4fe49
Fred Czerwinski <frederick.czerwinski@gmail.com>
2024-02-29 13:10:50 -0500

    home page
    home page

---
+++ frontend/src/app/home/page.js
@@ -0,0 +1,114 @@
+import "./home.css";
+
+export default function login(){
+    return( 
+<div className="home-page">
+<header>
+  <div>Ucache$ - Home</div>
+  <section class="balance">
+    <div>Balance: $2000.00</div>
+  </section>
+</header>
+<main>
+  <section className="navBar">
+    <div href="#">Profile</div>
+    <div href="#">Set Spend Limit</div>
+    <div href="#">Set Savings Goal</div>
+    <div href="#">View Child Account</div>
+  </section>
+  <section></section>
+  <section className="spendReport">
+    <div className="balChange increase">
+      <a className="balName">Walmartaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa</a>
+      <a className="balAmount">+$50.00</a>
+    </div>
+
+    <div className="balChange decrease">
+      <a className="balName">Shell</a>
+      <a className="balAmount">-$50.00</a>
+    </div>
+
+    <div className="balChange increase">
+      <a className="balName">Walmart</a>
+      <a className="balAmount">+$50.00</a>
+    </div>
+
+    <div className="balChange decrease">
+      <a className="balName">Shell</a>
+      <a className="balAmount">-$50.00</a>
+    </div>
+
+    <div className="balChange increase">
+      <a className="balName">Walmart</a>
+      <a className="balAmount">+$50.00</a>
+    </div>
+
+    <div className="balChange decrease">
+      <a className="balName">Shell</a>
+      <a className="balAmount">-$50.00</a>
+    </div>
+
+    <div className="balChange increase">
+      <a className="balName">Walmart</a>
+      <a className="balAmount">+$50.00</a>
+    </div>
+
+    <div className="balChange decrease">
+      <a className="balName">Shell</a>
+      <a className="balAmount">-$50.00</a>
+    </div>
+
+    <div className="balChange increase">
+      <a className="balName">Walmart</a>
+      <a className="balAmount">+$50.00</a>
+    </div>
+
+    <div className="balChange decrease">
+      <a className="balName">Shell</a>
+      <a className="balAmount">-$50.00</a>
+    </div>
+
+    <div className="balChange increase">
+      <a className="balName">Walmart</a>
+      <a className="balAmount">+$50.00</a>
+    </div>
+
+    <div className="balChange decrease">
+      <a className="balName">Shell</a>
+      <a className="balAmount">-$50.00</a>
+    </div>
+
+    <div className="balChange increase">
+      <a className="balName">Walmart</a>
+      <a className="balAmount">+$50.00</a>
+    </div>
+
+    <div className="balChange decrease">
+      <a className="balName">Shell</a>
+      <a className="balAmount">-$50.00</a>
+    </div>
+
+    <div className="balChange increase">
+      <a className="balName">Walmart</a>
+      <a className="balAmount">+$50.00</a>
+    </div>
+
+    <div className="balChange decrease">
+      <a className="balName">Shell</a>
+      <a className="balAmount">-$50.00</a>
+    </div>
+
+    <div className="balChange increase">
+      <a className="balName">Walmart</a>
+      <a className="balAmount">+$50.00</a>
+    </div>
+
+    <div className="balChange decrease">
+      <a className="balName">Shell</a>
+      <a className="balAmount">-$50.00</a>
+    </div>
+  </section>
+</main>
+</div>
+    );
+}
