e6f7fd675a115b433a3429c214d5220e2e74e0ec
Corey Aras Kaulenas <corey.kaulenas@gmail.com>
2024-02-29 12:21:45 -0500

    Renamed branch

--- backend/auth/views.py
+++ backend/auth/views.py
@@ -1,5 +1,7 @@
 from django.http import JsonResponse
 from django.views.decorators.csrf import csrf_exempt
+from django.contrib.auth.models import User
+from django.contrib.auth import authenticate
 from django.views.decorators.http import require_http_methods
 import json
 
@@ -17,7 +19,8 @@ def login(request):
 
         # Perform authentication logic here (e.g., check credentials against database)
         # Replace the following logic with your actual authentication logic
-        if username == "admin" and password == "123":
+        user = authenticate(username=username, password=password)
+        if user is not None:
             return JsonResponse({"success": True})
         else:
             return JsonResponse({"success": False})
@@ -26,5 +29,16 @@ def login(request):
 
 @require_http_methods(["POST", "OPTIONS"])
 @csrf_exempt
-def signup():
-    pass
+def signup(request):
+    if request.method == "POST":
+        # Parse the JSON data from the request body
+        data = json.loads(request.body.decode("utf-8"))
+
+        # Retrieve username and password from the JSON data
+        username = data.get("username")
+        password = data.get("password")
+
+        # create user with username and password
+        # None is email, could change with time
+        user = User.objects.create_user(username, None, password)
+        user.save()
