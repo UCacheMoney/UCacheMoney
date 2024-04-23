30b18c1a021b304f6f16e394cd3ac255db281e36
Bradan Craig <bradancraig2004@gmail.com>
2024-04-23 14:14:12 -0400

    Have pie charts and Bar graphs to visualize spending with hard coded values. Will need a method to retrieve categoeis and spending

--- frontend/src/app/graphs/page.js
+++ frontend/src/app/graphs/page.js
@@ -1,38 +1,5 @@
-//"use client"
-import {BarGraphObserver, PieGraphObserver, LineGraphObserver} from './graphs'
-import BarGraph from './attempt2'
-// import dynamic from 'next/dynamic';
-// import { Chart} from "chart.js";
-// const DynamicBarGraph = dynamic(() => import('./attempt2'),{
-// ssr: false
-// })
-
-// class Observable{ //creating Observable class to alert observers
-//     constructor() {
-//         this.observers = [];
-//     }
-
-//     addObj(obj) {
-//         this.observers.push(obj);
-//     }
-
-//     delObj(obj) {
-//         this.observers = this.observers.filter((observer) => observer !== obj);
-//     }
-
-//     notify(data){
-//         for(let i = 0; i != (this.observers).length; i++){
-//             (this.observers[i]).display(data)
-//         }
-//     }
-// }
-// const Observer = new Observable()
-// /*const BarObserver = new BarGraphObserver()
-// const LineObserver = new LineGraphObserver()
-// const PieObserver = new PieGraphObserver()
-// Observer.addObj(BarObserver)
-// Observer.addObj(LineObserver)//adds each subclass to the observable list
-// Observer.addObj(PieObserver)*/
+import BarGraph from './components/barGraph'
+import PieGraph from './components/pieGraph'
 
 export default function GraphPage(){
 //     function pieClicked(){
@@ -44,41 +11,20 @@ export default function GraphPage(){
 //     }
 
 //     function barClicked(){
-//         bar
+//         
 //     }
 
-//     const COLORS = ['rgb(255, 99, 132)', 'rgb(201, 203, 207)', 'rgb(153, 102, 255)', 'rgb(255, 205, 86)']
-//     const categories = ['Gas', 'Food', 'Rent', 'Fun']
-//     const spending = [300, 200, 1000, 54]
-//     const limits = [500, 300, 1500, 100]
     
-//     let BarGraph = new Chart(document.getElementById('graphsCanvas'), {
-//         type: "bar",
-//         data: {
-//             labels: categories,
-//             datasets: []
-//         },
-//         options: {}
-//     });
-    
-//     for (let i = 0; i <= categories.length; i++){
-//         let newDataset= {
-//             label: categories[i],
-//             borderColor: COLORS[i],
-//             data: spending[i]
-//         }
-//         BarGraph.data.datasets.push(newDataset)
-//         BarGraph.update()
-//     }
 
     return (
-//create index file and import as document to be refrenced
             <div className="Graph Page">
                     <a>You are on the graph page</a>
                     <main>
                         <body>
                         <section className="body">
+                            <button onClick={PieGraph}>Bar Graph</button>
                             <BarGraph/>
+                            <PieGraph/>
                         </section>
                         </body>
                     </main>
